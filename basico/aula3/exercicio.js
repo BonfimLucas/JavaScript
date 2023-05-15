81484c5f809915a98a3fc9b5bb5121db1c61a51d
Lucas <lucasbonfim604@gmail.com>
2023-05-14 21:39:15 -0300

    aula3

---
+++ basico/aula3/exercicio.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Lucas Bonfim". Estrou aprendendo JavaScript as', 21,'horas da noite');
---
+++ basico/aula3/index.js
@@ -0,0 +1,5 @@
+console.log("Hello 'world'"); // string
+console.log('Hello "World"'); // string
+
+console.log(1234567, 15.85, 'Hello World'); // number
+
