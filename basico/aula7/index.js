7a348c5d735b044bc14288803fe1cabf675950b2
Lucas <lucasbonfim604@gmail.com>
2023-05-15 21:56:40 -0300

    aula7

---
+++ basico/aula7/index.js
@@ -0,0 +1,13 @@
+// +, -, *, /
+// Strings = Text
+// Number = Número
+const primeiroNumero = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(resultado, resultadoDuplicado, resultadoTriplicado);
+
+
+console.log(typeof(primeiroNumero + segundoNumero));
