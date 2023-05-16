f8fb55861eb2ba30f1980d982f0c33788cc46ada
Lucas <lucasbonfim604@gmail.com>
2023-05-15 21:24:08 -0300

    aula6

---
+++ basico/aula5/index.js
@@ -0,0 +1,5 @@
+// Um comentário
+console.log('Olá Mundo!');
+console.log('Este codigo sera exibido no console do navegador!');
+alert('Hello World');
+alert('Meu nome é Lucas');
---
+++ basico/aula6/index.js
@@ -0,0 +1,33 @@
+let nome = 'João'; //String.
+let sobrenome; // Declarou variavel.
+
+sobrenome = 'Bonfim'; // Inicializando variavel.
+console.log(sobrenome);
+sobrenome = 'Santos';
+console.log(sobrenome);
+
+// Não podemos criar variáveis com palavras reservadas.
+// Variáveis precisam ter nomes significativos.
+
+let nomeCliente = 'João';
+console.log(nomeCliente);
+
+// Não pode começar o nome de uma variavel com um número.
+
+let no1me;
+
+// Não podem conter espaços ou traços.
+
+// Utilizar camelCase.
+
+let nomeCompleto = 'Lucas Bonfim';
+console.log(nomeCompleto);
+
+// Variaveis são case-sensitive.
+
+let nomeCliente1 = 'Lucas';
+let nomecliente1 = 'Bonfim';
+
+console.log(nomeCliente1, nomecliente1);
+
+//Não utilize VAR, utilize LET.
