70b1d7cf2ac7c004b768494e9e8db5aa65e39b82
Lucas <lucasbonfim604@gmail.com>
2023-07-10 19:20:35 -0300

    aula13

---
+++ basico/aula12/script.js
@@ -0,0 +1,10 @@
+const confirma = confirm('Realmente deseja apagar?');
+
+alert('Ola mundo');
+window.prompt('Digite seu nome:');
+
+let num1 = prompt('Digite um número: ');
+let num2 = prompt('Digite outro número: ')
+
+window.prompt(num1);
+window.prompt(num2);
---
+++ basico/aula13/script.js
@@ -0,0 +1,45 @@
+//               0123456789
+let umaString = 'O rato roeu a roupa do rei de roma';
+
+
+//---Acessar indice de uma string.---
+// console.log(umaString[4]);
+//console.log(umaString.charAt(8));
+
+
+//---Concatenar uma string.---
+//console.log(umaString.concat(' em',' um', ' lindo dia.'));
+//console.log(umaString + ' em um lindo dia.');
+//console.log(`${umaString} em um lindo dia.`);
+
+
+//---localizar caractere de uma string.---
+//console.log(umaString.indexOf('o', 3));
+//console.log(umaString.lastIndexOf('m', 3));
+//console.log(umaString.match(/[a-z]/g));
+//console.log(umaString.search(/x/));
+
+
+//---Substituir caractere de uma string.---
+//console.log(umaString.replace(/r/g, '#'));
+
+
+//---Tamanho de uma string.---
+//console.log(umaString.length);
+
+
+
+//---Fatiamento de uma string.---
+//console.log(umaString.slice(2, 6));
+// console.log(umaString.length -3);
+// console.log(umaString.slice(-3));
+// console.log(umaString.slice(32));
+// console.log(umaString.slice(-5, -1));
+// console.log(umaString.substring(umaString.length -5, umaString.length -1));
+//console.log(umaString.split(' ', 3));
+
+
+
+//----Converter para tamanhos diferentes.---
+//console.log(umaString.toUppercase());
+//console.log(umaString.toLowercase());
