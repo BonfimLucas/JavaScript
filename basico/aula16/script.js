6afbbebd9fc040b4a559340758a3d489f72d3eac
Lucas <lucasbonfim604@gmail.com>
2023-07-11 21:52:16 -0300

    Arrays em Javascript

---
+++ basico/aula16/script.js
@@ -0,0 +1,26 @@
+
+//                0       1         2
+let alunos = ['Luiz', 'Maria', 'João'];
+alunos = 123;
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+//alunos.push('Luiza'); //
+//alunos.push('Eduardo'); //
+
+//console.log(alunos.slice(0, -1)); // fatia de tras pra frente
+//console.log(alunos.slice(0, 3)); // fatia de acordo com os parametros
+
+//alunos[0] = 'Eduardo'; // altera
+//alunos[3] = 'Jessica'; // adiciona
+//alunos.push('Lucas'); // adiciona no fim
+//alunos.unshift('Fernanda'); // adiciona no primeiro
+//alunos.pop('João') // remover do final
+//alunos.shift('João') // adicionar
+
+//alunos[alunos.length] = 'Luiza' // adiciona
+//alunos[alunos.length] = 'Fabio' // adiciona
+//alunos[alunos.length] = 'Luana' // adiciona
+
+//console.log(removido);
