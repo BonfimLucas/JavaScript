9d6839eff285a9937e51e3590469b8d03609f842
Lucas <lucasbonfim604@gmail.com>
2023-07-12 21:33:37 -0300

    função

---
+++ basico/aula17/script.js
@@ -0,0 +1,15 @@
+function soma(num1 = 1, num2 = 1) {
+  const resultado = num1 + num2;
+  return resultado;    
+};
+
+const resultado = soma(2,2);
+console.log(resultado);
+
+
+const raiz = (n) => n ** 0.5;
+
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
